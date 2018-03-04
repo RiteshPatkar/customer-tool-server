f3554e62b6831ded1216d30fef049e4cc5e13494
RiteshPatkar <riteshkptkar@gmail.com>
2018-03-03 21:36:20 -0500

    Solved cors issue

--- src/main/java/com/wipro/customertool/interceptor/CORSInterceptor.java
+++ src/main/java/com/wipro/customertool/interceptor/CORSInterceptor.java
@@ -1,26 +1,22 @@
 package com.wipro.customertool.interceptor;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 @Component
 public class CORSInterceptor extends HandlerInterceptorAdapter {
 	
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		response.addHeader("Access-Control-Allow-Origin", "*");
-		return true;
-	}
+//	@Override
+//	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+//			throws Exception {
+//		response.addHeader("Access-Control-Allow-Origin", "*");
+//		return true;
+//	}
 	
-	@Override
-	public void postHandle(
-			HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
-			throws Exception {
-		response.addHeader("Access-Control-Allow-Origin", "*");
-	}
+//	@Override
+//	public void postHandle(
+//			HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
+//			throws Exception {
+//		response.addHeader("Access-Control-Allow-Origin", "*");
+//	}
 }
--- src/main/java/com/wipro/customertool/main/ServiceConfiguration.java
+++ src/main/java/com/wipro/customertool/main/ServiceConfiguration.java
@@ -1,28 +1,31 @@
 package com.wipro.customertool.main;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.wipro.customertool.interceptor.CORSInterceptor;
 
 @Configuration
 @EnableWebMvc
 public class ServiceConfiguration extends WebMvcConfigurerAdapter {
 	
-//	@Bean
-//	CORSInterceptor corsInterceptor() {
-//		return new CORSInterceptor();
-//	}
-//	
-//	@Override
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		registry.addInterceptor(corsInterceptor());
-//	}
+	@Bean
+	CORSInterceptor corsInterceptor() {
+		return new CORSInterceptor();
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(corsInterceptor());
+	}
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-//			.allowedOrigins("http://domain2.com")
 			.allowedMethods("PUT", "DELETE", "GET", "POST")
 			.allowCredentials(false).maxAge(3600);
 	}
